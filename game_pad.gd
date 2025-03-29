8cd4c8813e8ca7eb89bd526a28dba9e3033e2ab2
bird guy.cs <120428688+birdguy-cs@users.noreply.github.com>
2025-03-28 23:40:53 -0700

    Update game_pad.gd

--- game_pad.gd
+++ game_pad.gd
@@ -44,46 +44,37 @@ func is_inside_circle1(pos: Vector2) -> bool:
 func _input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
 		if event.pressed:
-			print("finger no: ", event.index, " is pressing")
-			print(event.position)
 			if is_inside_circle0(event.position):
-				print("circle0 touch")
 				circle0_pressing = true
 				circle0_index = event.index
 				circle0_position = event.position
 			elif is_inside_circle1(event.position):
-				print("circle1 touch")
 				circle1_pressing = true
 				circle1_index = event.index
 				circle1_position = event.position
 		else:
-			print("finger no: ", event.index, " is released")
 			if event.index == circle0_index: 
 				circle0_pressing = false
 				circle0_index = -1
-				circle0_position = event.position
 			elif event.index == circle1_index: 
 				circle1_pressing = false
 				circle1_index = -1
-				circle1_position = event.position
 	elif event is InputEventScreenDrag:
 		if event.index == circle0_index:
-			print("circle0 drag")
 			circle0_position = event.position
 		elif event.index == circle1_index: 
-			print("circle1 drag")
 			circle1_position = event.position
 
 # Update positions of circles when pressing or dragging
 func _process(delta: float) -> void:
 	if circle0_pressing:
 		var screen_pos = get_screen_position(circle0_relative_position)
-		print((screen_pos - circle0_position).normalized())
-		$Sprite.position += -(screen_pos - circle0_position).normalized() * 500 * delta
+		# Move circle0's position based on drag
+		circle0_position = screen_pos
 	if circle1_pressing:
 		var screen_pos = get_screen_position(circle1_relative_position)
-		print((screen_pos - circle1_position).normalized())
-		$Sprite.scale += (screen_pos - circle1_position).normalized() * 10 * delta
+		# Move circle1's position based on drag
+		circle1_position = screen_pos
 
 # Draw the circles on screen
 func _draw() -> void:
